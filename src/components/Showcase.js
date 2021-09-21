9881e692b20812a4931674ed45a48b9fbcab6f64
MalboroAdvanced <brijesh.1995rana@gmail.com>
2021-09-22 01:49:58 +0530

    done

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -1,22 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const Showcase = () => {
-    return (
-        <div className="show">
-            <div className="container rel">
-           <div className="show__content">
-               <h1>Sothat'sus.There'sn</h1>
-               <h1>ootherwaytoputit.</h1>
-               </div>
-
-               <div className="show__card">
-                   <h6 className="show__card-h6">OUR HAPPY CLIENTS</h6>
-                   <div className="show__card-h4">What Client's Say?</div>
-                   <div className="show__card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. At sequi dolor, facere alias praesentium officiis labore mollitia saepe, accusamus nam deleniti veritatis.</div>
-               </div>
-               </div> 
+  return (
+    <div className="show">
+      <div className="container rel">
+        <div className="show__content">
+          <h1>So that's us.There's no</h1>
+          <h1>other way to put it.</h1>
         </div>
-    )
-}
 
-export default Showcase
+        <div className="show__card">
+          <h6 className="show__card-h6">OUR HAPPY CLIENTS</h6>
+          <div className="show__card-h4">What Client's Say?</div>
+          <div className="show__card-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At sequi
+            dolor, facere alias praesentium officiis labore mollitia saepe,
+            accusamus nam deleniti veritatis.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Showcase;
